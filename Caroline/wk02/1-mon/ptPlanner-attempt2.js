1ab072f852c2e9a3aaa9bd6f5ed2a9e6f6dccfe0
Caroline <vc.setiawan@gmail.com>
2018-06-24 22:42:12 +1000

    pt planner hw

---
+++ Caroline/wk02/1-mon/ptPlanner-attempt2.js
@@ -0,0 +1,128 @@
+console.log('PT Planner attempt 2')
+
+var trainLines = [
+    {
+        lineName: 'Alamein',
+        stations: 'Flinders Street, Richmond, East Richmond, Burnley, Hawthorn, Glenferrie'.split(', ')
+    },
+    {
+        lineName: 'Glen Waverley',
+        stations: 'Flagstaff, Melbourne Central, Parliament, Richmond, Kooyong, Tooronga'.split(', ')
+    },
+    {
+        lineName: 'Sandringham',
+        stations: 'Southern Cross, Richmond, South Yarra, Prahran, Windsor'.split(', ')
+    }
+]
+
+//Probs could just have one function for isValid..
+// Take input for origin/destination & the prompt message?
+var isValid = function(){
+    if (trainLines[0].stations.includes(origin) === false){
+        if (trainLines[1].stations.includes(origin) === false){
+            if (trainLines[2].stations.includes(origin) === false){
+                origin = prompt('SORRY TRY AGAIN - ORIGIN')
+                isValid()
+            }
+        }
+    }
+}
+
+var isValid2 = function(){
+    if (trainLines[0].stations.includes(destination) === false){
+        if (trainLines[1].stations.includes(destination) === false){
+            if (trainLines[2].stations.includes(destination) === false){
+                destination = prompt('SORRY TRY AGAIN - DESTINATION')
+                isValid2()
+            }
+        }
+    }
+}
+
+//var origin = 'Windsor'
+var origin = prompt('please enter your station of origin')
+isValid()
+ 
+//var destination = 'Richmond'
+var destination = prompt('please enter your destination station')
+isValid2()
+
+var originLine
+var destinationLine
+
+for (var a=0; a<trainLines.length; a++){
+    if (trainLines[a].stations.indexOf(origin)>=0){
+        originLine = trainLines[a]
+    }
+}
+
+for (var b=0; b<trainLines.length; b++){
+    if (trainLines[b].stations.indexOf(destination)>=0){
+        destinationLine = trainLines[b]
+    } 
+}
+
+var printDeets = function(journey){
+    console.log('origin: ' + journey[0])
+    console.log('destination: ' + journey[journey.length-1] + '\n')
+    console.log(journey.join(' -----> ') + '\n')
+    console.log(journey.length-1 + ' stops total')
+}
+
+if (originLine === destinationLine){
+    var line = originLine
+    
+    if (line.stations.indexOf(origin)>line.stations.indexOf(destination)){
+        line = line.stations.reverse()
+    }
+
+    var originIndex = line.indexOf(origin)
+    var destinationIndex = line.indexOf(destination)
+    var newJourney = line.slice(originIndex,destinationIndex+1)
+
+    printDeets(newJourney)
+    
+} else {
+
+    var toRichmond = originLine.stations
+    var fromRichmond = destinationLine.stations
+
+    if (toRichmond.indexOf(origin)>toRichmond.indexOf('Richmond')) {
+        toRichmond = originLine.stations.reverse()
+        console.log('reversing the originLine')
+    }
+        
+    var originIndex = toRichmond.indexOf(origin)
+    var firstLeg = toRichmond.slice(originIndex,toRichmond.indexOf('Richmond'))
+    console.log('normal originLine')
+        
+    if (fromRichmond.indexOf(destination)<fromRichmond.indexOf('Richmond')){
+        fromRichmond = destinationLine.stations.reverse()
+        console.log('reversing destinationLine')
+    }
+    
+    var destinationIndex = fromRichmond.indexOf(destination)
+    var secondLeg = fromRichmond.slice(fromRichmond.indexOf('Richmond'),destinationIndex+1)
+    
+    var newJourney = firstLeg.concat(secondLeg)
+    console.log('normal destinationLine')
+    printDeets(newJourney)
+} 
+
+// how to add additional stations. push.
+// create addStation function
+// specify line
+// and then station
+var findLine = function (lineToFind){
+    for (var x=0; x<trainLines.length; x++){
+        if(trainLines[x].lineName === lineToFind){
+            return trainLines[x]
+        }
+    }
+}
+
+var addStation = function(lineToFind,stationToAdd){
+    findLine(lineToFind).stations.push(stationToAdd)
+    console.log(findLine(lineToFind))
+}
+
--- Caroline/wk02/1-mon/ptPlanner.js
+++ Caroline/wk02/1-mon/ptPlanner.js
@@ -1,22 +1,125 @@
 console.log('PT Planner fun yay')
 
-//remember join and split
+var alamein = 'Flinders Street, Richmond, East Richmond, Burnley, Hawthorn, Glenferrie'.split(', ')
+var glenWaverley = 'Flagstaff, Melbourne Central, Parliament, Richmond, Kooyong, Tooronga'.split(', ')
+var sandringham = 'Southern Cross, Richmond, South Yarra, Prahran, Windsor'.split(', ')
+var trainLines = [alamein,glenWaverley,sandringham]
 
-var alamein = 'Flinders Street, Richmond, East Richmond, Burnley, Hawthorn, Glenferrie'.split(',')
-var glenWaverley = 'Flagstaff, Melbourne Central, Parliament, Richmond, Kooyong, Tooronga'.split(',')
-var sandringham = 'Southern Cross, Richmond, South Yarra, Prahran, Windsor'.split(',')
+// WHAT TO DO
+// Find which line origin station belongs to. Record it. 
+// Find which line destination station belongs to. Record it.
+// If they belong to the same line, run printDeets function.
+// If not, somehow slice and join the two lines(arrays) and then run printDeets function?
 
-// Start with one train line first. 
-// display the origin & destination
-// display journey (the stops) when we give it origin and destination (use join for dashes)
-// display how many stops total
+// Input validation. Need two loops. 
+// One to keep bringing up the prompt.
+//One to check if userInput belongs in one of the train lines. 
 
-var origin = 'Richmond'
-var destination = 'Burnley'
+//how do i combine these two into one function......
+var isValid = function(){
+    if (trainLines[0].includes(origin) === false){
+        if (trainLines[1].includes(origin) === false){
+            if (trainLines[2].includes(origin) === false){
+                origin = prompt('SORRY TRY AGAIN - ORIGIN')
+                isValid()
+            }
+        }
+    }
+}
 
-console.log(origin)
-console.log(destination)
+var isValid2 = function(){
+    if (trainLines[0].includes(destination) === false){
+        if (trainLines[1].includes(destination) === false){
+            if (trainLines[2].includes(destination) === false){
+                destination = prompt('SORRY TRY AGAIN - DESTINATION')
+                isValid2()
+            }
+        }
+    }
+}
 
-// how do we relate user input with the info we have stored for each line??
-// use match??!
+//var origin = 'Windsor'
+var origin = prompt('please enter your station of origin')
+isValid()
+ 
+//var destination = 'Richmond'
+var destination = prompt('please enter your destination station')
+isValid2()
 
+
+
+var originLine
+var destinationLine
+
+for (var a=0; a<trainLines.length; a++){
+    if (trainLines[a].indexOf(origin)>=0){
+        originLine = trainLines[a]
+    }
+}
+
+for (var b=0; b<trainLines.length; b++){
+    if (trainLines[b].indexOf(destination)>=0){
+        destinationLine = trainLines[b]
+    } 
+}
+
+var printDeets = function(journey){
+    console.log('origin: ' + journey[0])
+    console.log('destination: ' + journey[journey.length-1] + '\n')
+    console.log(journey.join(' -----> ') + '\n')
+    console.log(journey.length-1 + ' stops total')
+}
+
+if (originLine === destinationLine){
+    var line = originLine
+    
+    if (line.indexOf(origin)>line.indexOf(destination)){
+        line = line.reverse()
+    }
+
+    var originIndex = line.indexOf(origin)
+    var destinationIndex = line.indexOf(destination)
+    var newJourney = line.slice(originIndex,destinationIndex+1)
+
+    printDeets(newJourney)
+    
+} else {
+
+    // origin to richmond
+    // if origin > richmond, reverse
+    // slice
+    // richmond to destination
+    // if destination < richmond, reverse
+    // slice
+    // concat
+    var toRichmond = originLine
+    var fromRichmond = destinationLine
+
+    if (toRichmond.indexOf(origin)>toRichmond.indexOf('Richmond')) {
+        toRichmond = originLine.reverse()
+        //console.log('reversing the originLine')
+    }
+        
+    var originIndex = toRichmond.indexOf(origin)
+    var firstLeg = toRichmond.slice(originIndex,toRichmond.indexOf('Richmond'))
+    //console.log('normal originLine')
+        
+    if (fromRichmond.indexOf(destination)<fromRichmond.indexOf('Richmond')){
+        fromRichmond = destinationLine.reverse()
+        //console.log('reversing destinationLine')
+    }
+    
+    var destinationIndex = fromRichmond.indexOf(destination)
+    var secondLeg = fromRichmond.slice(fromRichmond.indexOf('Richmond'),fromRichmond.indexOf(destination)+1)
+    
+    var newJourney = firstLeg.concat(secondLeg)
+    //console.log('normal destinationLine')
+    printDeets(newJourney)
+} 
+
+// OK fine.. how to add additional stations. push.
+// create addStation function
+// specify line
+// and then station
+
+// ^ See attempt 2 (ptPlanner-attempt2.js)
