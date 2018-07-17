8e0a12a7427d5a03a9eb9e7687c5501878c26ebb
Caroline <vc.setiawan@gmail.com>
2018-07-18 00:20:19 +1000

    sql queries & movies part II

--- Caroline/wk05/1-mon/movies/main.rb
+++ Caroline/wk05/1-mon/movies/main.rb
@@ -8,9 +8,19 @@ get '/' do
   erb :index
 end
 
-get '/movie' do
+get '/movie/:movie_title' do
   @result = HTTParty.get("http://omdbapi.com/?t=#{params[:movie_title]}&apikey=2f6435d9")
   erb :movie
+end
+
+get '/movielist' do
+  @list = HTTParty.get("http://omdbapi.com/?s=#{params[:movie_title]}&apikey=2f6435d9")
+  if @list["Search"].length == 1
+    redirect to ("/movie/#{params[:movie_title]}")
+    #movie is being changed to movielist mmmmm :/
+  end
+  # binding.pry
+  erb :movielist
 end
 
 get '/about' do
