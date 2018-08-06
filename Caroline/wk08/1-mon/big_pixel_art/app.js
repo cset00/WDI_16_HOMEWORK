b6a93636961862c537110cfb12edb4e608ad610a
Caroline <vc.setiawan@gmail.com>
2018-08-06 16:44:40 +1000

    1000 divs of class: square appended to body

--- Caroline/wk08/1-mon/big_pixel_art/app.js
+++ Caroline/wk08/1-mon/big_pixel_art/app.js
@@ -5,3 +5,15 @@ setColorBtn.on('click', function(event){
     console.log('CLICK')
     //change color of 'brush' box to color i specify in input field
 })
+
+
+
+var createSquares = function (){
+    
+    for (var i=0; i<1000; i++){
+        var div = $('<div>').addClass('square')
+        $('.container').append(div)
+    }
+}
+
+createSquares()
