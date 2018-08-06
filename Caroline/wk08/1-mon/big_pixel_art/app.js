e86e964aea238b4c4d306246aa1cd51e8fe6459c
Caroline <vc.setiawan@gmail.com>
2018-08-06 17:26:24 +1000

    commit 7: added a form to allow users to pick a movie poster using omdb api and set the BG to it

--- Caroline/wk08/1-mon/big_pixel_art/app.js
+++ Caroline/wk08/1-mon/big_pixel_art/app.js
@@ -1,10 +1,12 @@
 setColorBtn = $('.set-color-btn')
 container = $('.container')
+movieBtn = $('.bg-btn')
+movieInput = $('.movie')
 
 
 setColorBtn.on('click', function(event){
     event.preventDefault()
-    console.log('CLICK')
+    console.log('color clicked')
     //change color of 'brush' box to color i specify in input field
 })
 
@@ -17,9 +19,30 @@ var createSquares = function (){
 
 createSquares()
 
+
+
 container.on('mouseover', '.square', function(){
     // $(event.target).addClass('change-color')
-    $(event.target).css("background-color", `${$('input').val()}`)
+    $(event.target).css("background-color", `${$('.color-input').val()}`)
+})
+
+
+movieBtn.on('click', function(event){
+    event.preventDefault()
+    console.log('movie clicked')
+
+    
+
+    function onSuccess(responseData){
+        console.log(responseData)
+        container.css("background", `url(${responseData["Poster"]})`)
+    }
+
+    $.ajax({
+        method: 'GET',
+        url: 'http://www.omdbapi.com/?t=' + movieInput.val() + '&apikey=2f6435d9&',
+        dataType: 'json'
+    }).done(onSuccess)
 })
 
 
