a1c4b03b9daeba292c426d1cf537ec084405586f
Caroline <vc.setiawan@gmail.com>
2018-08-06 16:32:52 +1000

    set up form for users to input color

--- Caroline/wk07/4-thu/giphy/app.js
+++ Caroline/wk07/4-thu/giphy/app.js
@@ -3,7 +3,7 @@ console.log('hello')
 var searchBtn = $('.search-btn')
 var results = $('.container')
 var searchBar = $('.search-bar')
-var number = 0
+var offset = 0
 
 searchBtn.click(function(){
     
@@ -18,7 +18,12 @@ searchBtn.click(function(){
     }
     $.ajax({
         method: 'GET',
-        url: 'http://api.giphy.com/v1/gifs/search?q=' + searchBar.val() + '&api_key=2PJzovJQxTppU9ePAOacPJIYhgiCst03',
+        url: 'http://api.giphy.com/v1/gifs/search',
+        data: {
+            q: searchBar.val(),
+            offset: offset,
+            api_key: '2PJzovJQxTppU9ePAOacPJIYhgiCst03'
+        },
         dataType: 'json',
     }).done(onSuccess)
 })
@@ -27,7 +32,7 @@ searchBtn.click(function(){
 $(window).scroll(function () { 
     if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
         console.log('SCROLLINGGGG')
-        number = number + 25
+        offset = offset + 25
         
         function onSuccess(responseData) {
             for (let i=0;i<responseData["data"].length;i++){
@@ -40,8 +45,12 @@ $(window).scroll(function () {
         
         $.ajax({
             method: 'GET',
-            url: 'http://api.giphy.com/v1/gifs/search?q=' + searchBar.val() + '&offset=' + number + '&api_key=2PJzovJQxTppU9ePAOacPJIYhgiCst03',
+            url: 'http://api.giphy.com/v1/gifs/search?q=' + searchBar.val() + '&offset=' + offset + '&api_key=2PJzovJQxTppU9ePAOacPJIYhgiCst03',
             dataType: 'json',
         }).done(onSuccess)
     }
+ })
+
+ $('.back-to-top').click(function(){
+    document.scrollingElement.scrollTo({top:0, behavior: 'smooth'})
  })
---
+++ Caroline/wk08/1-mon/big_pixel_art/app.js
@@ -0,0 +1,7 @@
+setColorBtn = $('.set-color-btn')
+
+setColorBtn.on('click', function(event){
+    event.preventDefault()
+    console.log('CLICK')
+    //change color of 'brush' box to color i specify in input field
+})
